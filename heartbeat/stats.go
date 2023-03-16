da2241c502a9977f899b621995e3ef4b272280f4
R.I.Pienaar <rip@devco.net>
2023-03-16 10:45:37 +0100

    (#83) document heartbeats based monitoring
    Also update statistic names to be consistent with
    current names
    
    Signed-off-by: R.I.Pienaar <rip@devco.net>

--- heartbeat/stats.go
+++ heartbeat/stats.go
@@ -11,23 +11,23 @@ import (
 var (
 	// gauge
 	hbSubjects = prometheus.NewGaugeVec(prometheus.GaugeOpts{
-		Name: prometheus.BuildFQName("choria_stream_replicator", "replicator", "hb_subjects"),
+		Name: prometheus.BuildFQName("choria_stream_replicator", "heartbeat", "subjects_count"),
 		Help: "Number of subjects that heartbeats are being published for",
 	}, []string{"replicator", "subject"})
 	hbPublishedCtr = prometheus.NewCounterVec(prometheus.CounterOpts{
-		Name: prometheus.BuildFQName("choria_stream_replicator", "replicator", "hb_published_ctr"),
+		Name: prometheus.BuildFQName("choria_stream_replicator", "heartbeat", "published_count"),
 		Help: "Number of published messages",
 	}, []string{"replicator", "subject"})
 	hbPublishedCtrErr = prometheus.NewCounterVec(prometheus.CounterOpts{
-		Name: prometheus.BuildFQName("choria_stream_replicator", "replicator", "hb_published_error_ctr"),
+		Name: prometheus.BuildFQName("choria_stream_replicator", "heartbeat", "published_error_count"),
 		Help: "Number of published message errors",
 	}, []string{"replicator", "subject"})
 	hbPublishTime = prometheus.NewSummaryVec(prometheus.SummaryOpts{
-		Name: prometheus.BuildFQName("choria_stream_replicator", "replicator", "hb_publish_time"),
+		Name: prometheus.BuildFQName("choria_stream_replicator", "heartbeat", "publish_time"),
 		Help: "Time taken to publish a message",
 	}, []string{"replicator", "subject"})
 	hbPaused = prometheus.NewGaugeVec(prometheus.GaugeOpts{
-		Name: prometheus.BuildFQName("choria_stream_replicator", "replicator", "hb_paused"),
+		Name: prometheus.BuildFQName("choria_stream_replicator", "heartbeat", "paused"),
 		Help: "Paused under leader election",
 	}, []string{"replicator"})
 )
